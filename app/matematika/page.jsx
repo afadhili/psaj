ad5df52d9b8ff31add963bedc77869ed6dd603ef
L1HD4F <afadhili03@gmail.com>
2024-03-19 00:06:26 +0700

    first commit

---
+++ app/matematika/page.jsx
@@ -0,0 +1,16 @@
+import Navbar from "@/components/layouts/Navbar"
+
+export default function Matematika() {
+  return (
+    <main>
+
+      <Navbar />
+
+      <div className="container mx-auto">
+        <iframe src="/mtk/mtk-1.pdf" className="w-full h-screen my-8" ></iframe>
+        <iframe src="/mtk/mtk-2.pdf" className="w-full h-screen my-8" ></iframe>
+      </div>
+
+    </main>
+  )
+}
---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/layouts/Navbar";
+
+export default function Home() {
+  return (
+    <main>
+
+      <Navbar />
+
+      <h1 className="text-3xl font-bold text-center">PSAJ Test</h1>
+
+    </main>
+  );
+}
